e0a2af6c376f7bd7808590cd896ec540ec37792d
伊東和成 <itokazunari5375@icloud.com>
2022-08-26 08:30:58 +0900

    feat: CRUDのテストを追加

--- spec/requests/posts_apis_spec.rb
+++ spec/requests/posts_apis_spec.rb
@@ -25,3 +25,36 @@ describe 'GET /posts/:id' do
     expect(json["title"]).to eq(post["title"])
   end
 end
+
+describe 'Post /posts' do
+  it '新しいタスクを作成する' do
+    valid_params = { title: "test"}
+
+    #データが作成されていることを確認
+    expect { post '/posts', params: valid_params }.to change(Post, :count).by(+1)
+    expect(response.status).to eq(201)
+  end
+end
+
+describe "PUT /posts/:id" do
+  it 'ユーザーが更新されること' do
+    post = create(:post)
+    post_update_params = {
+      title: "更新です"
+    }
+    put "/posts/#{post.id}", params: post_update_params
+
+    expect(response.status).to eq(200)
+    expect(post.reload.title).to eq(post_update_params[:title])
+  end
+end
+
+describe 'Delete /posts/:id' do
+  it 'postを削除する' do
+    post = create(:post)
+    #データが削除されている事を確認
+    expect { delete "/posts/#{post.id}" }.to change(Post, :count).by(-1)
+    # リクエスト成功を表す200が返ってきたか確認する。
+    expect(response.status).to eq(204)
+  end
+end
