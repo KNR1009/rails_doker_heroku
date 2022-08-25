8f2d6689bd383e4e06a14182e2f7749f0d51e5c9
伊東和成 <itokazunari5375@icloud.com>
2022-08-26 08:05:56 +0900

    feat: 新規作成用のテストを作成

--- spec/requests/posts_apis_spec.rb
+++ spec/requests/posts_apis_spec.rb
@@ -13,3 +13,15 @@ describe 'GET /posts' do
     expect(json.length).to eq(10)
   end
 end
+
+describe 'GET /posts/:id' do
+  it '特定の記事を取得する' do
+    post = create(:post, title: "サンプル記事")
+    get "/posts/#{post.id}"
+    json = JSON.parse(response.body)
+    # # リクエスト成功を表す200が返ってきたか確認する。
+    expect(response.status).to eq(200)
+    # # 正しい数のデータが返されたか確認する。
+    expect(json["title"]).to eq(post["title"])
+  end
+end
